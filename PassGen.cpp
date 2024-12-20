d41c814ed055ddaab83a54777cbde87b93fb83c2
Kareem Farrell <kareemfarrell306@gmail.com>
2024-12-20 19:21:33 -0400

    Added User Prompts / Removed sss.txt

--- PassGen.cpp
+++ PassGen.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
+
+int main() {
+	
+	int length;
+	char op1, op2, op3, op4;
+	string sequence;
+	
+	cout << "--- Welcome to PassGen ---\n\n";
+	
+	cout << "Enter the Length of your password: ";
+	cin >> length;
+	
+	cout << "Would you like to include characters A-Z? [Yes/No]\n\n";
+	cin >> op1;
+	
+	cout << "Would you like to include characters 0-9? [Yes/No]\n\n";
+	cin >> op2;
+	
+	cout << "Would you like to include special characters !@#? [Yes/No]\n\n";
+	cin >> op3;
+	
+	cout << "Would you like your password to include a specific sequence. [Yes/No]\n";
+	cout << "Sequence: blueberry\n";
+	cout << "Password: =3blueberry!?a\n";
+	cin >> op4;
+}
 
