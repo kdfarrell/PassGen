b0838c703e8f0a93bb3e204124f6701345d10513
Kareem Farrell <kareemfarrellonline@gmail.com>
2024-12-25 19:16:22 -0400

    User Prompt Updated

--- PassGen.cpp
+++ PassGen.cpp
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <unordered_set>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,24 +19,19 @@ using namespace std;
 #define CYAN "\033[0;36m"
 #define BRIGHT_YELLOW "\033[1;33m"
 
-// Function to display a message with color
-void printMessage(const string& message, const string& color) {
-    cout << color << message << RESET << endl;
-}
-
 int main() {
 	
-	char choices[6];
+	vector<char> charSet;
 	char choice;
 	int length, amount;
 	
-	printMessage("--- Welcome to PassGen ---", BLUE);
+	cout << BLUE << "--- Welcome to PassGen ---\n" << RESET;
     cout << CYAN << "This program will help you create a secure and random password.\n";
     cout << CYAN << "You will be able to customize your password based on various criteria.\n";
     cout << CYAN << "Let's get started!\n\n" << RESET;
     
     // Choosing the amount of passwords
-	printMessage("How many passwords would you like to generate?", CYAN);
+	cout << CYAN << ("How many passwords would you like to generate?\n") << RESET;
     cout << BRIGHT_YELLOW << "Please enter a number between 1 and 10 (inclusive): " << RESET;
     
     while (true) {
@@ -44,20 +40,20 @@ int main() {
 		if (cin.fail()) { // user enters non-numeric character
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			printMessage("\nInvalid input. Please enter a number between 1 and 10 (inclusive).", RED);
+			cout << RED << "\nInvalid input. Please enter a number between 1 and 10 (inclusive).\n" << RESET;
 			cout << BRIGHT_YELLOW << "Length: " << RESET;
 		}
 		else if (amount >= 1 && amount <= 10) {
             break;
         } 
 		else {
-            printMessage("Invalid input. Please enter a number between 1 and 10 (inclusive).", RED);
+            cout << RED << "Invalid input. Please enter a number between 1 and 10 (inclusive).\n" << RESET;
             cout << BRIGHT_YELLOW << "Length: " << RESET;
         }
 	}
 
 	// Choosing the length of the password
-	printMessage("\nHow long would you like your password to be?", CYAN);
+	cout << CYAN << "\nHow long would you like your password to be?\n"  << RESET;
     cout << BRIGHT_YELLOW << "Please enter a number between 8 and 32 (inclusive): " << RESET;
 	
 	while (true) {
@@ -66,124 +62,166 @@ int main() {
 		if (cin.fail()) { // user enters non-numeric character
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			printMessage("\nInvalid input. Please enter a number between 8 and 32 (inclusive).", RED);
+			cout << RED << "\nInvalid input. Please enter a number between 8 and 32 (inclusive).\n" << RESET;
 			cout << BRIGHT_YELLOW << "Length: " << RESET;
 		}
 		else if (length >= 8 && length <= 32) {
             break;
         } 
 		else {
-            printMessage("Invalid input. Please enter a number between 8 and 32 (inclusive).", RED);
+            cout << RED << "\nInvalid input. Please enter a number between 8 and 32 (inclusive).\n" << RESET;
             cout << BRIGHT_YELLOW << "Length: " << RESET;
         }
 	}
 	
-	// Choosing letters a-z
-	printMessage("\nInclude characters (a-z)?", CYAN);
-    cout << YELLOW << "[Y] Yes" << RESET << endl;
-    cout << YELLOW << "[N] No" << RESET << endl;
-    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-    cin >> choice;
-    choice = toupper(choice);
-    
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	while (choice != 'Y' && choice != 'N') {
-        printMessage("\nInvalid input. Please enter a 'Y' or 'N'.", RED);
-        cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-        cin >> choice;
-        choice = toupper(choice);
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    choices[0] = choice;
+	bool populated = false;
 	
-	//Choosing letters A-Z
-	printMessage("\nInclude characters (A-Z)?", CYAN);
-    cout << YELLOW << "[Y] Yes" << RESET << endl;
-    cout << YELLOW << "[N] No" << RESET << endl;
-    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-    cin >> choice;
-    choice = toupper(choice);
-    
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	while (choice != 'Y' && choice != 'N') {
-        printMessage("\nInvalid input. Please enter a 'Y' or 'N'.", RED);
-        cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-        cin >> choice;
-        choice = toupper(choice);
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    choices[1] = choice;
-	
-	//Choosing numbers 0-9
-	printMessage("\nInclude characters (0-9)?", CYAN);
-    cout << YELLOW << "[Y] Yes" << RESET << endl;
-    cout << YELLOW << "[N] No" << RESET << endl;
-    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-    cin >> choice;
-    choice = toupper(choice);
-    
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	while (choice != 'Y' && choice != 'N') {
-        printMessage("\nInvalid input. Please enter a 'Y' or 'N'.", RED);
-        cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-        cin >> choice;
-        choice = toupper(choice);
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    choices[2] = choice;
-	
-	//Choosing special characters
-	printMessage("\nInclude special characters (*%#!)?", CYAN);
-    cout << YELLOW << "[Y] Yes" << RESET << endl;
-    cout << YELLOW << "[N] No" << RESET << endl;
-    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-    cin >> choice;
-    choice = toupper(choice);
-    
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	while (choice != 'Y' && choice != 'N') {
-        printMessage("\nInvalid input. Please enter a 'Y' or 'N'.", RED);
-        cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-        cin >> choice;
-        choice = toupper(choice);
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    choices[3] = choice;
-	
-	//Choosing to exclude characters
-	printMessage("\nWould you like to exclude any characters?", CYAN);
-    cout << YELLOW << "[Y] Yes" << RESET << endl;
-    cout << YELLOW << "[N] No" << RESET << endl;
-    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-    cin >> choice;
-    choice = toupper(choice);
-    
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	while (choice != 'Y' && choice != 'N') {
-        printMessage("\nInvalid input. Please enter a 'Y' or 'N'.", RED);
-        cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
-        cin >> choice;
-        choice = toupper(choice);
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    choices[4] = choice;
-	
-	string exclude;
-	unordered_set<char> excluded;
-    if (choices[4] == 'Y') {
-        cout << BRIGHT_YELLOW << "\nWhat characters would you like to exclude? ";
-        cout << "\nCharacters: " << RESET;
-        cin.ignore();
-        getline(cin, exclude);
-    }
-    
-    for (auto c : exclude) {
-    	excluded.insert(c);
+	while (populated == false) {
+
+		// Choosing letters a-z
+		cout << CYAN << "\nInclude characters (a-z)?\n" << RESET;
+		cout << YELLOW << "[Y] Yes" << RESET << endl;
+		cout << YELLOW << "[N] No" << RESET << endl;
+		cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+		cin >> choice;
+		choice = toupper(choice);
+		
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		while (choice != 'Y' && choice != 'N') {
+		    cout << RED << "\nInvalid input. Please enter a 'Y' or 'N'.\n" << RESET;
+		    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+		    cin >> choice;
+		    choice = toupper(choice);
+		    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		
+		if (choice == 'Y') {
+			for (int i = 97; i <= 122; i++) {
+				charSet.push_back(i);
+			}
+			populated = true;
+		}
+		
+		//Choosing letters A-Z
+		cout << CYAN << "\nInclude characters (A-Z)?\n" << RESET;
+		cout << YELLOW << "[Y] Yes" << RESET << endl;
+		cout << YELLOW << "[N] No" << RESET << endl;
+		cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+		cin >> choice;
+		choice = toupper(choice);
+		
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		while (choice != 'Y' && choice != 'N') {
+		    cout << RED << "\nInvalid input. Please enter a 'Y' or 'N'.\n" << RESET;
+		    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+		    cin >> choice;
+		    choice = toupper(choice);
+		    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		
+		if (choice == 'Y') {
+			for (int i = 65; i <= 90; i++) {
+				charSet.push_back(i);
+			}
+			populated = true;
+		}
+		
+		//Choosing numbers 0-9
+		cout << CYAN << "\nInclude characters (0-9)?\n" << RESET;
+		cout << YELLOW << "[Y] Yes" << RESET << endl;
+		cout << YELLOW << "[N] No" << RESET << endl;
+		cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+		cin >> choice;
+		choice = toupper(choice);
+		
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		while (choice != 'Y' && choice != 'N') {
+		    cout << RED << "\nInvalid input. Please enter a 'Y' or 'N'.\n" << RESET;
+		    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+		    cin >> choice;
+		    choice = toupper(choice);
+		    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		
+		if (choice == 'Y') {
+			for (int i = 48; i <= 57; i++) {
+				charSet.push_back(i);
+			}
+			populated = true;
+		}
+		
+		//Choosing special characters
+		cout << CYAN << "\nInclude special characters (*%#!)?\n" << RESET;
+		cout << YELLOW << "[Y] Yes" << RESET << endl;
+		cout << YELLOW << "[N] No" << RESET << endl;
+		cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+		cin >> choice;
+		choice = toupper(choice);
+		
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		while (choice != 'Y' && choice != 'N') {
+		    cout << RED << "\nInvalid input. Please enter a 'Y' or 'N'.\n" << RESET;
+		    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+		    cin >> choice;
+		    choice = toupper(choice);
+		    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		
+		if (choice == 'Y') {
+			for (int i = 33; i <= 47; i++) {
+				charSet.push_back(i);
+			}
+			
+			for (int i = 58; i <= 64; i++) {
+				charSet.push_back(i);
+			}
+			
+			for (int i = 91; i <= 96; i++) {
+				charSet.push_back(i);
+			}	
+			
+			for (int i = 123; i <= 126; i++) {
+				charSet.push_back(i);
+			}
+			populated = true;	
+		}
+		
+		if (populated == false) {
+			cout << RED << "\nInvalid input: You didn't select any charachters. You must choose at least 1 charachter set.\n" << RESET;
+		}
 	}
+	
+	// Using Similar Characters
+	cout << CYAN << "\nExclude similar-looking characters, including 'i', 'l', '1', 'L', 'o', '0', and 'O'.?\n" << RESET;
+    cout << YELLOW << "[Y] Yes" << RESET << endl;
+    cout << YELLOW << "[N] No" << RESET << endl;
+    cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+    cin >> choice;
+    choice = toupper(choice);
     
-    
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	while (choice != 'Y' && choice != 'N') {
+        cout << RED << "\nInvalid input. Please enter a 'Y' or 'N'.\n" << RESET;
+        cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
+        cin >> choice;
+        choice = toupper(choice);
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+       
+    if (choice == 'Y') {
+    	string similar = "il1Lo0OI|!";
+    	
+    	for (int i = 0 ; i < similar.length(); i++) {
+    		auto itr = find(charSet.begin(), charSet.end(), similar[i]);
+    			
+			if (itr != charSet.end()) {
+				itr = charSet.erase(itr);
+			}
+		}		
+	}
+   	
 	// Include sequence of characters
-	printMessage("\nWould you like to include a specific sequence of characters?", CYAN);
+	cout << CYAN << "\nWould you like to use the same character more than once?\n" << RESET;
     cout << CYAN << "Example: ('1234', 'abc') in your password?" << RESET << endl;
     cout << YELLOW << "[Y] Yes" << RESET << endl;
     cout << YELLOW << "[N] No" << RESET << endl;
@@ -193,133 +231,20 @@ int main() {
     
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	while (choice != 'Y' && choice != 'N') {
-        printMessage("\nInvalid input. Please enter a 'Y' or 'N'.", RED);
+        cout << RED << "\nInvalid input. Please enter a 'Y' or 'N'.\n" << RESET;
         cout << BRIGHT_YELLOW << "Your choice (Y/N): " << RESET;
         cin >> choice;
         choice = toupper(choice);
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
-    choices[5] = choice;
-	
-	string sequence;
-    if (choices[5] == 'Y') {
-        cout << BRIGHT_YELLOW << "\nWhat characters would you like to include in the sequence? ";
-    	cout << "\nCharacters: " << RESET;
-        cin.ignore();
-        getline(cin, sequence);
-        
-        while (sequence.length() > length) {
-        	printMessage("\nInvalid input. Sequence is longer than length of password", RED);
-        	cout << BRIGHT_YELLOW << "Enter Sequence: " << RESET;
-        	cin.ignore();
-        	getline(cin, sequence);
-		}
-    }
     
-    printMessage("\nThank you for your input! Your password will be generated based on your selections.", GREEN);
-	
-	srand(time(NULL));
-	vector<string> allPasswords;
-	string password = "";
-	char selectedChar;
-	int temp = length;
-	
-	if (choices[5] == 'Y') {	
-		password = password + sequence;
-		length = length - sequence.length();
-	}
-	
-	while (length != 0) {
-		
-		int chartype = rand() % 4;
-		
-		if (chartype == 0 && choices[0] != 'N') {
-			selectedChar = rand() % (122 - 97 + 1) + 97; // a-z
-			
-			while (excluded.find(selectedChar) != excluded.end()) {
-				selectedChar = rand() % (122 - 97 + 1) + 97;
-			}
-			password = selectedChar + password;	
-			length--;
-		}
-		else if (chartype == 1 && choices[1] != 'N') {
-			selectedChar = rand() % (90 - 65 + 1) + 65; // A-Z
-			
-			while (excluded.find(selectedChar) != excluded.end()) {
-				selectedChar = rand() % (90 - 65 + 1) + 65;
-			}
-			password = selectedChar + password;	
-			length--;
-		}
-		else if (chartype == 2 && choices[2] != 'N') { // 0-9
-			selectedChar = rand() % (57 - 48 + 1) + 48;
-			
-			while (excluded.find(selectedChar) != excluded.end()) {
-				selectedChar = rand() % (57 - 48 + 1) + 48;
-			}
-			password = selectedChar + password;	
-			length--;
-		}
-		else if (chartype == 3 && choices[3] != 'N') { //Special Characters
-		
-			int typeOfSpecial = rand() % 4 + 1;
-			
-			if (typeOfSpecial == 1) {
-				selectedChar = rand() % (47 - 33 + 1) + 33;
-			
-				while (excluded.find(selectedChar) != excluded.end()) {
-					selectedChar = rand() % (47 - 33 + 1) + 33;
-				}
-				password = selectedChar + password;	
-				length--;	
-			}
-			else if (typeOfSpecial == 2) {
-				selectedChar = rand() % (64 - 58 + 1) + 58;
-			
-				while (excluded.find(selectedChar) != excluded.end()) {
-					selectedChar = rand() % (64 - 58 + 1) + 58;
-				}
-				password = selectedChar + password;	
-				length--;
-			}
-			else if (typeOfSpecial == 3) {
-				selectedChar = rand() % (96 - 91 + 1) + 91;
-			
-				while (excluded.find(selectedChar) != excluded.end()) {
-					selectedChar = rand() % (96 - 91 + 1) + 91;
-				}
-				password = selectedChar + password;	
-				length--;
-			}
-			else if (typeOfSpecial == 4) {
-				selectedChar = rand() % (126 - 123 + 1) + 123;
-			
-				while (excluded.find(selectedChar) != excluded.end()) {
-					selectedChar = rand() % (126 - 123 + 1) + 123;
-				}
-				password = selectedChar + password;	
-				length--;
-			}
-		}
-		
-		if (length == 0 && allPasswords.size() != amount) {
-			length = temp;
-			allPasswords.push_back(password);
-			password = "";
-			
-			if (choices[5] == 'Y') {	
-				password = password + sequence;
-				length = length - sequence.length();
-			}
-		}	
-	}
-	
-	cout << GREEN << "\nYour Password/s:\n" << password;
-	
-	for(auto word : allPasswords) {
-		cout << word << endl;
-	}
+    bool duplicate = false;
     
+    if (choice == 'Y') {
+    	duplicate = true;
+	}
+	
+    cout << GREEN << "\nThank you for your input! Your password will be generated based on your selections.\n" << RESET;
 	
 	return 0;
 }
